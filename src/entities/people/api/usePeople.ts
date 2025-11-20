26686aa02ce925b0dd7a8dc5f02a252074c6b4c0
Check Stanislau <fkrjcjdjifkdgdjsjjfmknckdnc@gmail.com>
2025-11-20 11:10:29 +0100

    refactor: changed default value

--- src/entities/people/api/usePeople.ts
+++ src/entities/people/api/usePeople.ts
@@ -2,12 +2,13 @@ import { AllPeopleSchema, type Person } from "../model/types";
 import { graphqlFetcher } from "@/entities/people/api/client";
 import { ALL_PEOPLE_QUERY } from "./queries";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { DEFAULT_FIRST_PEOPLE, DEFAULT_STALE_TIME_MS } from "@/shared/config/constants";
 
 export type UsePeopleParams = {
     first?: number;
 }
 
-const fetchPeople = async ({ first = 30 }: UsePeopleParams): Promise<Person[]> => {
+const fetchPeople = async ({ first = DEFAULT_FIRST_PEOPLE }: UsePeopleParams): Promise<Person[]> => {
     const data = await graphqlFetcher(ALL_PEOPLE_QUERY, { first });
     const parsed = AllPeopleSchema.parse(data);
     return parsed.allPeople.people;
@@ -17,6 +18,6 @@ export const usePeople = (params?: UsePeopleParams) => {
     return useSuspenseQuery<Person[]>({
         queryKey: ["people", params],
         queryFn: () => fetchPeople(params ?? {}),
-        staleTime: 1000 * 60 * 5,
+        staleTime: DEFAULT_STALE_TIME_MS,
     });
 };
--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -1,3 +1,4 @@
 export const DEFAULT_LIMIT = 4
 export const DEFAULT_RETRY_COUNT = 1
 export const DEFAULT_STALE_TIME_MS = 60_000 * 10
+export const DEFAULT_FIRST_PEOPLE = 30
